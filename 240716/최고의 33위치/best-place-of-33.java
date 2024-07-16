97aa0585dccbce158d5669f96ebda7d39e1adeaf
sw1029 <130366914+sw1029@users.noreply.github.com>
2024-07-16 15:57:45 +0900

    Title: 최고의 33위치, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240716/최고의 33위치/best-place-of-33.java
+++ 240716/최고의 33위치/best-place-of-33.java
@@ -1,49 +1,50 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
-        // 여기에 코드를 작성해주세요.
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final int MAX_N = 20;
 
-        String[] inp = br.readLine().split(" ");
-        int n = Integer.parseInt(inp[0]);
-        //int m = Integer.parseInt(inp[1]);
-        String[][] block = new String[n][n];
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
 
-        for (int i = 0; i < n; i++) {
-            block[i] = br.readLine().split(" ");
-        }
+    // (rowS, colS) ~ (rowE, colE) 사이의 금의 개수를 계산합니다.
+    public static int getNumOfGold(int rowS, int colS, int rowE, int colE) {
+        int numOfGold = 0;
 
-        int max = 0;
-
-        if (n <= 3) {
-            for (int i = 0; i < n; i++) {
-                String[] nums = block[i];
-                for (int j = 0; j < n; j++) {
-                    if (nums[j].equals("1")) {
-                        max += 1;
-                    }
-                }
-            }
-            System.out.print(max);
-            return;
-        }
-
-        for (int i = 1; i < n - 1; i++) {
-            String[] bnums = block[i - 1];
-            String[] nums = block[i];
-            String[] anums = block[i + 1];
-
-            for (int j = 1; j < n - 1; j++) {
-                int sum = 0;
-                if (nums[j - 1].equals("1")) sum += 1;
-                if (nums[j + 1].equals("1")) sum += 1;
-                if (anums[j].equals("1")) sum += 1;
-                if (bnums[j].equals("1")) sum += 1;
-                max = Math.max(max, sum);
+        for(int row = rowS; row <= rowE; row++) {
+            for(int col = colS; col <= colE; col++) {
+                numOfGold += grid[row][col];
             }
         }
+        
+        return numOfGold;
+    }
 
-        System.out.print(max);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int maxGold = 0;
+
+        // 입력
+        n = sc.nextInt();
+        for(int row = 0; row < n; row++)
+            for(int col = 0; col < n; col++)
+                grid[row][col] = sc.nextInt();
+        
+        // (row, col)이 3 * 3 격자의 좌측 모서리인 경우를 전부 탐색합니다. 
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < n; col++) {
+                // 3 * 3 격자가 n * n 격자를 벗어나는 경우는 계산하지 않습니다.
+                if(row + 2 >= n || col + 2 >= n)
+                    continue;
+                
+                // 금의 개수를 계산합니다.
+                int numOfGold = getNumOfGold(row, col, row + 2, col + 2);
+
+                // 최대 금의 개수를 저장합니다.
+                maxGold = Math.max(maxGold, numOfGold);
+            }
+        }
+
+        System.out.println(maxGold);
     }
 }
