fb9e7619daaba9e5b5d75ae7a7ef4c673d6c4319
sw1029 <130366914+sw1029@users.noreply.github.com>
2024-07-16 15:56:57 +0900

    Title: 최고의 33위치, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240716/최고의 33위치/best-place-of-33.java
+++ 240716/최고의 33위치/best-place-of-33.java
@@ -3,32 +3,47 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine()), max = 0;
-        String [] block = new String[n];
-        for(int i = 0; i < 3; i++)block[i]=br.readLine();
-        if(n<=3){
-            for(int i = 0; i < n; i++){
-                String[] nums = block[i].split(" ");
-                for(int j = 0; j < n; j++)if(nums[j].equals("1"))max+=1;
+        String[] inp = br.readLine().split(" ");
+        int n = Integer.parseInt(inp[0]);
+        //int m = Integer.parseInt(inp[1]);
+        String[][] block = new String[n][n];
+
+        for (int i = 0; i < n; i++) {
+            block[i] = br.readLine().split(" ");
+        }
+
+        int max = 0;
+
+        if (n <= 3) {
+            for (int i = 0; i < n; i++) {
+                String[] nums = block[i];
+                for (int j = 0; j < n; j++) {
+                    if (nums[j].equals("1")) {
+                        max += 1;
+                    }
+                }
             }
             System.out.print(max);
             return;
         }
-        for(int i = 1; i < n-1;i++){
-            String[] bnums = block[i-1].split(" ");
-            String[] nums = block[i].split(" ");
-            String[] anums = block[i+1].split(" ");
-            for(int j = 1; j < n-1; j++){
+
+        for (int i = 1; i < n - 1; i++) {
+            String[] bnums = block[i - 1];
+            String[] nums = block[i];
+            String[] anums = block[i + 1];
+
+            for (int j = 1; j < n - 1; j++) {
                 int sum = 0;
-                if(nums[j-1].equals("1"))sum+=1;
-                if(nums[j+1].equals("1"))sum+=1;
-                if(anums[j].equals("1"))sum+=1;
-                if(bnums[j].equals("1"))sum+=1;
-                max = Math.max(max,sum);
+                if (nums[j - 1].equals("1")) sum += 1;
+                if (nums[j + 1].equals("1")) sum += 1;
+                if (anums[j].equals("1")) sum += 1;
+                if (bnums[j].equals("1")) sum += 1;
+                max = Math.max(max, sum);
             }
         }
+
         System.out.print(max);
     }
 }
