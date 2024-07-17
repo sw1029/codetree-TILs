c77cb97a56bcadda3f82ba755e7280c5e0ab361c
sw1029 <130366914+sw1029@users.noreply.github.com>
2024-07-17 21:57:13 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 195ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240717/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
+++ 240717/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -66,19 +66,7 @@ public class Main {
 				
 			}
 			
-			int count=0,l=-1,r=-1;
-			for(int p = 0; p < n; p++) {
-				for(int q = 0; q < n; q++) {
-					if(ballMap[p][q]==newBallMap[p][q]) {
-						count++;
-						if(count==1) {
-							l=p;r=q;
-						}
-					}
-					if(count>=2)newBallMap[p][q]=false;
-					if(count==2)newBallMap[l][r]=false;
-				}
-			}
+			
 			
 			ballMap = newBallMap;
 			
