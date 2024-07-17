99592a2b503c97f9558ad59400f18f34c27e5c41
sw1029 <130366914+sw1029@users.noreply.github.com>
2024-07-17 21:30:57 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 104ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240717/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
+++ 240717/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -14,13 +14,13 @@ public class Main {
 		
 		int [][]map = new int [n][n];
 		
-		for(int i = 0; i < n;i++) {
+		for(int i = 0; i < n;i++) {//map 만들기
 			String [] inp = br.readLine().split(" ");
 			for(int j = 0; j < n; j++) {
 				map[i][j] = Integer.parseInt(inp[j]);
 			}
 		}
-		for(int i = 0; i < m; i++) {
+		for(int i = 0; i < m; i++) {//공의 위치 지정
 			String [] inp = br.readLine().split(" ");
 			int x = Integer.parseInt(inp[0]), y = Integer.parseInt(inp[1]);
 			ballMap[x-1][y-1] = true;
@@ -28,7 +28,7 @@ public class Main {
 		
 		
 		for(int i = 0; i < t; i++) {
-			boolean [][] newBallMap = new boolean[n][n];
+			boolean [][] newBallMap = new boolean[n][n];//갱신된 공의 위치
 			
 			for(int j = 0; j < n; j++) {
 				for(int k = 0; k < n; k++) {
@@ -37,7 +37,7 @@ public class Main {
 						if(j!=0)max=Math.max(map[j-1][k], max);
 						if(j!=n-1)max=Math.max(map[j+1][k], max);
 						if(k!=0)max=Math.max(map[j][k-1], max);
-						if(k!=n-1)max=Math.max(map[j][k+1], max);
+						if(k!=n-1)max=Math.max(map[j][k+1], max);//최댓값 확인 후 상하좌우 순서로
 						
 						if(j!=0 && map[j-1][k]==max) {
 							if(newBallMap[j-1][k])newBallMap[j-1][k]=false;
@@ -54,6 +54,9 @@ public class Main {
 						else if(k!=n-1 && map[j][k+1]==max) {
 							if(newBallMap[j][k+1])newBallMap[j][k+1]=false;
 							else newBallMap[j][k+1] = true;
+						}else {
+							if(newBallMap[j][k])newBallMap[j][k]=false;
+							else newBallMap[j][k] = true;
 						}
 						
 						
