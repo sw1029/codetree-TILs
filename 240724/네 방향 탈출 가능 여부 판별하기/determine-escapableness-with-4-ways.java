76b32a18e20d06e779cdc11ff14d07260c0132d7
sw1029 <130366914+sw1029@users.noreply.github.com>
2024-07-24 16:09:29 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 2305ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240724/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] input = br.readLine().split(" ");
+        int n=Integer.parseInt(input[0]),m=Integer.parseInt(input[1]);
+        map = new boolean[n][m];
+        trv = new boolean[n][m];
+        for(int i = 0; i < n; i++) {
+        	String [] inp = br.readLine().split(" ");
+        	for(int j = 0; j < m; j++) {
+        		if(inp[j].equals("1"))map[i][j] = true;
+        	}
+        }
+        
+        Queue <block> qu = new LinkedList<>();
+        qu.add(new block(0,0));
+        int [] dx = {-1,0,1,0};
+        int [] dy = {0,1,0,-1};
+       
+        while(!qu.isEmpty()) {
+        	block now = qu.poll();
+        	trv[now.x][now.y] = true;
+        	for(int i = 0; i < 4; i ++) {
+        		int nx = now.x + dx[i];
+        		int ny = now.y + dy[i];
+        		if(nx>=0&&ny>=0 && nx< n && ny <m && map[nx][ny] && !trv[nx][ny]) {
+        			qu.add(new block(nx,ny));
+        		}
+        	}
+        }
+        int answer = 0;
+        if(trv[n-1][m-1])answer = 1;
+        System.out.println(answer);
+    }
+	static boolean [] [] map;
+	static boolean [] [] trv;
+}
+class block{
+	int x,y;
+	public block(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+}
