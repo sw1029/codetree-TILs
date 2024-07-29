a0436a6fee5698bbf300ec60c99764412529a970
sw1029 <130366914+sw1029@users.noreply.github.com>
2024-07-29 21:03:43 +0900

    Title: 최대 감소 부분 수열, Time: 163ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240729/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		nums = new int [n];
+		mlen = new int[n];
+		trv = new boolean[n];
+		String [] inp = br.readLine().split(" ");
+		for(int i = 0; i < n; i++) {
+			nums[n-1-i] = Integer.parseInt(inp[i]);
+		}
+		
+		mlen[n-1] = 1;
+		trv[n-1] = true;
+		
+		int max = 0;
+		for(int i = 0; i < n; i++) {
+			max = Math.max(lenCount(i), max);
+		}
+		System.out.println(max);
+    }
+	static int [] nums;
+	static int [] mlen;
+	static boolean [] trv;
+	
+	static int lenCount(int n) {
+		if(!trv[n]) {
+			trv[n] = true;
+			int max = 0;
+			for(int i = n+1; i < mlen.length;i++) {
+				if(nums[i] > nums[n]) {
+					max = Math.max(lenCount(i), max);
+				}
+			}
+			mlen[n] = max+1;
+		}
+		return mlen[n];
+	}
+}
