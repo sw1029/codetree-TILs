6d59f0c22963527fcc7334343055f15a487d4329
sw1029 <130366914+sw1029@users.noreply.github.com>
2024-07-29 16:18:16 +0900

    Title: 피보나치 수, Time: 158ms, Memory: 8MB, Status: Passed - CodeTree

--- 240729/피보나치 수/fibonacci-number.java
+++ 240729/피보나치 수/fibonacci-number.java
@@ -6,16 +6,19 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-        
-		System.out.println(pibo(n));
+        int sum = 2;
+        pibos.add(1);
+        pibos.add(1);
+        for(int i = 0; i < n-2; i++) {
+        	pibos.add(sum);
+        	sum += pibos.get(pibos.size()-2);
+        }
+		
+		System.out.println(pibos.get(n-1));
         
     }
+	static ArrayList<Integer>pibos = new ArrayList<>();
 	
-	static int pibo(int n) {
-		if(n <= 2)return 1;
-		
-		return pibo(n-2) + pibo(n-1);
-	}
 	
 	
 }
